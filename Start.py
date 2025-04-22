8462b833f55a3ef9d6573d8bd7b88fc0949ed0d2
Aiden Turner <57725407+ItzTheDodo@users.noreply.github.com>
2025-04-22 10:04:15 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -2,10 +2,6 @@ from src.FFTJ import FFTJAppManager
 
 __version__ = "DEV"
 
-# "current_file": "C:\\Users\\User\\OneDrive\\Documents\\Applications\\FlippyFlappingTheJ\\assets\\example.automaton"
-# "current_file": "/Users/aiden/Library/CloudStorage/OneDrive-Personal/Documents/Applications/FlippyFlappingTheJ/assets/example.automaton"
-
-
 def main() -> int:
     status = FFTJAppManager(__version__).status
     return status
